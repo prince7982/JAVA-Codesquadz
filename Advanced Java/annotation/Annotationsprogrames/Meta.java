53dbba1edb4320dec59ce4882caa1b6dc466ffc1
prince7982 <princekumar13810@gmail.com>
2025-06-10 00:33:06 +0530

    anootation program add on

---
+++ Advanced Java/annotation/Annotationsprogrames/Meta.java
@@ -0,0 +1,40 @@
+import java.lang.annotation.*;
+import java.lang.reflect.*;
+
+
+@Retention(RetentionPolicy.RUNTIME)
+@interface MyAnno
+{
+	String str();
+	int val();
+}
+
+public class Meta
+{
+	// Annotate a method.
+	@MyAnno(str="Annotation Example",val = 100)
+	public static void myMeth()
+	{
+		Meta ob = new Meta();
+		
+		try
+		{
+			Class c = ob.getClass();
+			
+			Method m = c.getMethod("myMeth");
+			
+			MyAnno anno = m.getAnnotation(MyAnno.class);
+			
+			System.out.println(anno.str()+" "+anno.val());
+		}
+		catch(NoSuchMethodException exc)
+		{
+			System.out.println("Method Not Found.");
+		}
+	}
+	
+	public static void main(String args[])
+	{
+		myMeth();
+	}
+}
